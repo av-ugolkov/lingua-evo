0a0662f0ccba4fd74bd22233d8f1b193f73b1bde
alexandr <ugolkov.prog@gmail.com>
2022-11-19 15:58:06 +0300

    clients

---
+++ clients/telegram/telegram.go
@@ -0,0 +1,92 @@
+package telegram
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+)
+
+type Client struct {
+	host     string
+	basePath string
+	client   http.Client
+}
+
+const (
+	getUpdatesMethod  = "getUpdates"
+	sendMessageMethod = "sendMessage"
+)
+
+func New(host string, token string) *Client {
+	return &Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
+
+func (c *Client) Updates(offset int, limit int) ([]Update, error) {
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+
+	data, err := c.doRequest(getUpdatesMethod, q)
+	if err != nil {
+		return nil, err
+	}
+
+	var res UpdatesResponse
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+
+	return res.Result, nil
+}
+
+func (c *Client) SendMessage(chatID int, text string, entities string) error {
+	q := url.Values{}
+	q.Add("chat_id", strconv.Itoa(chatID))
+	q.Add("text", text)
+	q.Add("reply_markup", entities)
+
+	_, err := c.doRequest(sendMessageMethod, q)
+	if err != nil {
+		return fmt.Errorf("telegram.SendMessage.doRequest: %w", err)
+	}
+	return nil
+}
+
+func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("telegram.doRequest.NewRequest: %w", err)
+	}
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("telegram.doRequest.Do: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("telegram.doRequest.ReadAll: %w", err)
+	}
+
+	return body, nil
+}
