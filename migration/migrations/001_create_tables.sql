6584ca19cd14e29087887091e31bd834fa7cee79
aleksandr <ugolkov.prog@gmail.com>
2023-08-08 22:39:54 +0300

    edit tables

--- migration/migrations/001_create_tables.sql
+++ migration/migrations/001_create_tables.sql
@@ -1,51 +1,64 @@
 -- +goose Up
-create table if not exists users (
-    id uuid default gen_random_uuid() primary key,
-    name text not null,
-    email text not null,
-    password_hash text not null,
-    last_visit date
-);
+CREATE TABLE IF NOT EXISTS
+    users (
+        id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password_hash TEXT NOT NULL,
+        last_visit date
+    );
 
-create table if not exists word(
-    id uuid default gen_random_uuid() not null primary key,
-    text text not null,
-    lang text not null
-);
-create unique index if not exists idx_unique_word__text_lang
-    on word (text, lang);
+CREATE UNIQUE INDEX IF not EXISTS idx_unique_users__name ON users (name);
 
+CREATE UNIQUE INDEX IF not EXISTS idx_unique_users__email ON users (email);
 
-create table if not exists language (
-    code text not null primary key,
-    lang text not null
-);
-create unique index if not exists idx_unique_languages__lang_code
-    on language (lang, code);
+CREATE TABLE IF NOT EXISTS
+    language (id bigserial PRIMARY KEY, code TEXT NOT NULL, lang TEXT NOT NULL);
 
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_language__lang_code ON language (lang, code);
 
-create table if not exists example (
-    id uuid default gen_random_uuid() not null primary key,
-    word_id uuid,
-    example text
-);
+CREATE TABLE IF NOT EXISTS
+    word (
+        id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
+        text TEXT NOT NULL,
+        lang_id int,
+        tags UUID[],
+        CONSTRAINT word_lang_id_fkey FOREIGN KEY (lang_id) REFERENCES language (id) ON DELETE CASCADE
+    );
 
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_word__text_lang_id ON word (text, lang_id);
 
-create table if not exists dictionary (
-    user_id uuid references users (id) not null,
-    original_word uuid references word (id) not null,
-    pronunciation text,
-    translate_word uuid[] not null,
-    examples uuid[]
-);
-create unique index if not exists idx_unique_dictionary__user_id_original_word
-    on dictionary (user_id, original_word);
+CREATE TABLE IF NOT EXISTS
+    example (
+        id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
+        word_id UUID NOT NULL,
+        example TEXT,
+        CONSTRAINT example_word_id_fkey FOREIGN KEY (word_id) REFERENCES word (id)
+    );
 
+CREATE TABLE IF NOT EXISTS
+    dictionary (
+        user_id UUID REFERENCES users (id) NOT NULL,
+        original_word UUID REFERENCES word (id) NOT NULL,
+        pronunciation TEXT,
+        translate_word UUID[] NOT NULL,
+        examples UUID[]
+    );
 
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_dictionary__user_id_original_word ON dictionary (user_id, original_word);
+
+CREATE TABLE IF NOT EXISTS
+    tag (id bigserial, tag TEXT);
 
 -- +goose Down
-drop table if exists users;
-drop table if exists language;
-drop table if exists example;
-drop table if exists word;
-drop table if exists dictionary;
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS language;
+
+DROP TABLE IF EXISTS example;
+
+DROP TABLE IF EXISTS word;
+
+DROP TABLE IF EXISTS dictionary;
+
+DROP TABLE IF EXISTS tag;
