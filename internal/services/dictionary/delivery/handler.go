e8c5edd79e6c7ca7c29236e63db50c337b612e06
aleksandr <ugolkov.prog@gmail.com>
2023-10-10 22:04:31 +0300

    add dictionary service

--- internal/services/dictionary/delivery/handler.go
+++ internal/services/dictionary/delivery/handler.go
@@ -2,23 +2,31 @@ package delivery
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
 	"net/http"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 
-	"lingua-evo/internal/services/word/entity"
+	"lingua-evo/internal/services/dictionary/dto"
+	"lingua-evo/internal/services/dictionary/entity"
+	"lingua-evo/internal/tools"
 )
 
 const (
-	addDictionary    = "/add_dictionary"
-	removeDictionary = "/remove_dictionary"
+	addDictionary    = "/dictionary/add"
+	deleteDictionary = "/dictionary/delete"
+	getDictionary    = "/dictionary/get"
+	getAllDictionary = "/dictionary/get_all"
 )
 
 type (
 	dictionarySvc interface {
-		AddDictionary(ctx context.Context, word *entity.Word) (uuid.UUID, error)
-		RemoveDictionary(ctx context.Context, lang string) (*entity.Word, error)
+		AddDictionary(ctx context.Context, userID uuid.UUID, name string) (uuid.UUID, error)
+		DeleteDictionary(ctx context.Context, userID uuid.UUID, name string) error
+		GetDictionary(ctx context.Context, userID uuid.UUID, name string) (uuid.UUID, error)
+		GetDictionaries(ctx context.Context, userID uuid.UUID) ([]*entity.Dictionary, error)
 	}
 
 	Handler struct {
@@ -39,13 +47,94 @@ func newHandler(dictionarySvc dictionarySvc) *Handler {
 
 func (h *Handler) register(r *mux.Router) {
 	r.HandleFunc(addDictionary, h.addDictionary).Methods(http.MethodPost)
-	r.HandleFunc(removeDictionary, h.removeDictionary).Methods(http.MethodPost)
+	r.HandleFunc(deleteDictionary, h.deleteDictionary).Methods(http.MethodPost)
+	r.HandleFunc(getDictionary, h.getDictionary).Methods(http.MethodPost)
+	r.HandleFunc(getAllDictionary, h.getAllDictionary).Methods(http.MethodPost)
 }
 
 func (h *Handler) addDictionary(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		_ = r.Body.Close()
+	}()
 
+	var data dto.DictionaryRequest
+	err := tools.CheckBody(w, r, &data)
+	if err != nil {
+		tools.SendError(w, http.StatusInternalServerError, fmt.Errorf("dictionary.delivery.Handler.addDictionary - check body: %v", err))
+		return
+	}
+
+	ctx := r.Context()
+	dictID, err := h.dictionarySvc.AddDictionary(ctx, data.UserID, data.Name)
+	if err != nil {
+		tools.SendError(w, http.StatusInternalServerError, fmt.Errorf("dictionary.delivery.Handler.addDictionary: %v", err))
+	}
+
+	_, _ = w.Write([]byte(dictID.String()))
 }
 
-func (h *Handler) removeDictionary(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) deleteDictionary(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		_ = r.Body.Close()
+	}()
 
+	var data dto.DictionaryRequest
+	err := tools.CheckBody(w, r, &data)
+	if err != nil {
+		tools.SendError(w, http.StatusInternalServerError, fmt.Errorf("dictionary.delivery.Handler.deleteDictionary - check body: %v", err))
+		return
+	}
+
+	ctx := r.Context()
+	err = h.dictionarySvc.DeleteDictionary(ctx, data.UserID, data.Name)
+	if err != nil {
+		tools.SendError(w, http.StatusInternalServerError, fmt.Errorf("dictionary.delivery.Handler.deleteDictionary: %v", err))
+	}
+
+	_, _ = w.Write([]byte("done"))
+}
+
+func (h *Handler) getDictionary(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		_ = r.Body.Close()
+	}()
+
+	var data dto.DictionaryRequest
+	err := tools.CheckBody(w, r, &data)
+	if err != nil {
+		tools.SendError(w, http.StatusInternalServerError, fmt.Errorf("dictionary.delivery.Handler.getDictionary - check body: %v", err))
+		return
+	}
+
+	ctx := r.Context()
+	dictID, err := h.dictionarySvc.GetDictionary(ctx, data.UserID, data.Name)
+	if err != nil {
+		tools.SendError(w, http.StatusInternalServerError, fmt.Errorf("dictionary.delivery.Handler.getDictionary: %v", err))
+	}
+
+	_, _ = w.Write([]byte(dictID.String()))
+}
+
+func (h *Handler) getAllDictionary(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		_ = r.Body.Close()
+	}()
+
+	var data uuid.UUID
+	err := tools.CheckBody(w, r, &data)
+	if err != nil {
+		tools.SendError(w, http.StatusInternalServerError, fmt.Errorf("dictionary.delivery.Handler.getAllDictionary - check body: %v", err))
+		return
+	}
+
+	ctx := r.Context()
+	dictionaries, err := h.dictionarySvc.GetDictionaries(ctx, data)
+	if err != nil {
+		tools.SendError(w, http.StatusInternalServerError, fmt.Errorf("dictionary.delivery.Handler.getAllDictionary: %v", err))
+	}
+
+	//TODO нужно возвращать сериализованные данные
+	slog.Info(fmt.Sprintf("count dictionaries: %d", len(dictionaries)))
+
+	_, _ = w.Write([]byte("done"))
 }
--- internal/services/dictionary/service/dictionary.go
+++ internal/services/dictionary/service/dictionary.go
@@ -2,14 +2,24 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
-	"github.com/gofrs/uuid"
+	"lingua-evo/internal/services/dictionary/entity"
+
+	"github.com/google/uuid"
+)
+
+var (
+	errCountDictionary = errors.New("too much dictionaries for user")
 )
 
 type (
 	repoDict interface {
-		AddDictionary(ctx context.Context, userID uuid.UUID, name string) (uuid.UUID, error)
-		DeleteDictionary(ctx context.Context, userID uuid.UUID, name string) error
+		AddDictionary(ctx context.Context, dict entity.Dictionary) error
+		DeleteDictionary(ctx context.Context, dict entity.Dictionary) error
+		GetDictionary(ctx context.Context, dict entity.Dictionary) (uuid.UUID, error)
+		GetDictionaries(ctx context.Context, userID uuid.UUID) ([]*entity.Dictionary, error)
 	}
 )
 
@@ -21,4 +31,62 @@ func NewDictionarySvc(repo repoDict) *DictionarySvc {
 	return &DictionarySvc{
 		repo: repo,
 	}
+}
+
+func (s *DictionarySvc) AddDictionary(ctx context.Context, userID uuid.UUID, name string) (uuid.UUID, error) {
+	dict := entity.Dictionary{
+		ID:     uuid.New(),
+		UserID: userID,
+		Name:   name,
+	}
+
+	dictionaries, err := s.repo.GetDictionaries(ctx, userID)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("dictionary.service.DictionarySvc.AddDictionary - get count dictionaries: %w", err)
+	}
+
+	if len(dictionaries) > 3 {
+		return uuid.Nil, fmt.Errorf("dictionary.service.DictionarySvc.AddDictionary - %w %v", errCountDictionary, userID)
+	}
+
+	err = s.repo.AddDictionary(ctx, dict)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("dictionary.service.DictionarySvc.AddDictionary: %w", err)
+	}
+
+	return dict.ID, nil
+}
+
+func (s *DictionarySvc) DeleteDictionary(ctx context.Context, userID uuid.UUID, name string) error {
+	dict := entity.Dictionary{
+		UserID: userID,
+		Name:   name,
+	}
+
+	err := s.repo.DeleteDictionary(ctx, dict)
+	if err != nil {
+		return fmt.Errorf("dictionary.service.DictionarySvc.DeleteDictionary: %w", err)
+	}
+	return nil
+}
+
+func (s *DictionarySvc) GetDictionary(ctx context.Context, userID uuid.UUID, name string) (uuid.UUID, error) {
+	dict := entity.Dictionary{
+		UserID: userID,
+		Name:   name,
+	}
+
+	dictID, err := s.repo.GetDictionary(ctx, dict)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("dictionary.service.DictionarySvc.GetDictionary: %w", err)
+	}
+	return dictID, nil
+}
+
+func (s *DictionarySvc) GetDictionaries(ctx context.Context, userID uuid.UUID) ([]*entity.Dictionary, error) {
+	dictionaries, err := s.repo.GetDictionaries(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("dictionary.service.DictionarySvc.GetDictionaries: %w", err)
+	}
+	return dictionaries, nil
 }
