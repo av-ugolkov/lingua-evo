6c3b1aa9b039801be609ee5c80fd86e4ce14d0ce
aleksandr <ugolkov.prog@gmail.com>
2023-10-12 22:14:29 +0300

    add role for user

--- migration/migrations/001_create_tables.sql
+++ migration/migrations/001_create_tables.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS
         name TEXT NOT NULL,
         email TEXT NOT NULL,
         password_hash TEXT NOT NULL,
+        role TEXT NOT NULL,
         last_visit date
     );
 
