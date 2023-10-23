ca8e40e46f09b4322eebadba5018f7ce085a2144
aleksandr <ugolkov.prog@gmail.com>
2023-10-23 20:13:36 +0300

    create header om the main page

--- website/main/script.js
+++ website/main/script.js
@@ -1,22 +1,25 @@
 
-window.onload = (event) => {
-    let lableRandom = document.getElementById("lable-random")
-    setInterval(function () {
-        fetch("/word/get_random", {
-            method: "post",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ language_code: 'en-GB' })
+let lableRandom = document.getElementById("lable-random")
+let interval = setInterval(function () {
+    fetch("/word/get_random", {
+        method: "post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ language_code: 'en-GB' })
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            lableRandom.textContent = data["text"]
         })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Success:', data["text"]);
-                lableRandom.textContent = data["text"]
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, 2000);
+        .catch((error) => {
+            lableRandom.textContent = error
+            stopInterval()
+        });
+}, 2000);
+
+
+function stopInterval() {
+    clearInterval(interval)
 }
