00831c0c5c2bf51a256cdba3fc7630ded5b31f0d
aleksandr <ugolkov.prog@gmail.com>
2023-10-29 21:27:01 +0200

    update refresh token

--- internal/services/auth/repository/repository.go
+++ internal/services/auth/repository/repository.go
@@ -28,6 +28,16 @@ func (r *SessionRepo) SetSession(ctx context.Context, s *entity.Session) error {
 	return nil
 }
 
+func (r *SessionRepo) GetSession(ctx context.Context, refreshToken uuid.UUID) (*entity.Session, error) {
+	var s entity.Session
+	query := `SELECT refresh_token, user_id, expires_at, created_at FROM session WHERE refresh_token=$1`
+	err := r.db.QueryRowContext(ctx, query, refreshToken).Scan(&s.RefreshToken, &s.UserID, &s.ExpiresAt, &s.CreatedAt)
+	if err != nil {
+		return nil, fmt.Errorf("auth.repository.SessionRepo.GetSession: %w", err)
+	}
+	return &s, nil
+}
+
 func (r *SessionRepo) GetCountSession(ctx context.Context, userID uuid.UUID) (int64, error) {
 	var count int64
 	query := `SELECT count(*) FROM session WHERE user_id=$1`
@@ -36,6 +46,15 @@ func (r *SessionRepo) GetCountSession(ctx context.Context, userID uuid.UUID) (in
 		return -1, fmt.Errorf("auth.repository.SessionRepo.GetCountSession: %w", err)
 	}
 	return count, nil
+}
+
+func (r *SessionRepo) DeleteSession(ctx context.Context, session uuid.UUID) error {
+	query := `DELETE FROM session WHERE refresh_token=$1`
+	_, err := r.db.ExecContext(ctx, query, session)
+	if err != nil {
+		return fmt.Errorf("auth.repository.SessionRepo.DeleteSession: %w", err)
+	}
+	return nil
 }
 
 func (r *SessionRepo) DeleteAllUserSessions(ctx context.Context, userID uuid.UUID) error {
