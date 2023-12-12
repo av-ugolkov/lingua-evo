721fc9d10f7da2929c86f9d594116abe3685051e
aleksandr <ugolkov.prog@gmail.com>
2023-12-12 22:48:35 +0200

    update account panel

--- website/main/script.js
+++ website/main/script.js
@@ -96,18 +96,3 @@ bntLogin.addEventListener("click", () => {
         console.log(data);
     })
 })
-
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.accountBtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
