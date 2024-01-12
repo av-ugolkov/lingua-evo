d67da6d226fe5f69affe6ebd7d48943c827cff98
aleksandr <ugolkov.prog@gmail.com>
2024-01-12 20:48:18 +0200

    remove site files

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,14 @@ import (
 
 	"lingua-evo/internal/app"
 	"lingua-evo/internal/config"
-	"lingua-evo/pkg/files"
 )
 
 func main() {
 	var configPath string
 	flag.StringVar(&configPath, "config", "./configs/server_config.yaml", "it's name of application config")
-	var webPath string
-	flag.StringVar(&webPath, "web_path", "./website", "it's path to static files")
-	var staticFilePath string
-	flag.StringVar(&staticFilePath, "static_web_path", "./", "it's path to static files")
 
 	flag.Parse()
 
-	files.InitStaticFiles(staticFilePath)
 	cfg := config.InitConfig(configPath)
-	app.ServerStart(cfg, webPath)
+	app.ServerStart(cfg)
 }
