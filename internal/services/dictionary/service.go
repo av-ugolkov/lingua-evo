bf43c426124756a1b020323ceb6e4b27149a612d
aleksandr <ugolkov.prog@gmail.com>
2024-02-07 11:52:11 +0200

    rename dictionary

--- internal/services/dictionary/service.go
+++ internal/services/dictionary/service.go
@@ -14,10 +14,11 @@ var (
 
 type (
 	repoDict interface {
-		AddDictionary(ctx context.Context, dict Dictionary) error
-		DeleteDictionary(ctx context.Context, dict Dictionary) error
-		GetDictionaryByName(ctx context.Context, dict Dictionary) (uuid.UUID, []uuid.UUID, error)
+		Add(ctx context.Context, dict Dictionary) error
+		Delete(ctx context.Context, dict Dictionary) error
+		GetByName(ctx context.Context, dict Dictionary) (uuid.UUID, []uuid.UUID, error)
 		GetDictionaries(ctx context.Context, userID uuid.UUID) ([]*Dictionary, error)
+		Rename(ctx context.Context, id uuid.UUID, newName string) error
 	}
 
 	repoVocab interface {
@@ -59,7 +60,7 @@ func (s *Service) AddDictionary(ctx context.Context, userID, dictID uuid.UUID, n
 		return uuid.Nil, fmt.Errorf("dictionary.Service.AddDictionary - %w %v", errCountDictionary, dictionary.UserID)
 	}
 
-	err = s.repoDict.AddDictionary(ctx, dictionary)
+	err = s.repoDict.Add(ctx, dictionary)
 	if err != nil {
 		return uuid.Nil, fmt.Errorf("dictionary.Service.AddDictionary: %w", err)
 	}
@@ -73,7 +74,7 @@ func (s *Service) DeleteDictionary(ctx context.Context, userID uuid.UUID, name s
 		Name:   name,
 	}
 
-	err := s.repoDict.DeleteDictionary(ctx, dict)
+	err := s.repoDict.Delete(ctx, dict)
 	if err != nil {
 		return fmt.Errorf("dictionary.Service.DeleteDictionary: %w", err)
 	}
@@ -86,7 +87,7 @@ func (s *Service) GetDictionary(ctx context.Context, userID uuid.UUID, name stri
 		Name:   name,
 	}
 
-	dictID, tags, err := s.repoDict.GetDictionaryByName(ctx, dict)
+	dictID, tags, err := s.repoDict.GetByName(ctx, dict)
 	if err != nil {
 		return uuid.Nil, nil, fmt.Errorf("dictionary.Service.GetDictionary: %w", err)
 	}
@@ -100,4 +101,12 @@ func (s *Service) GetDictionaries(ctx context.Context, userID uuid.UUID) ([]*Dic
 		return nil, fmt.Errorf("dictionary.Service.GetDictionaries: %w", err)
 	}
 	return dictionaries, nil
+}
+
+func (s *Service) RenameDictionary(ctx context.Context, id uuid.UUID, newName string) error {
+	err := s.repoDict.Rename(ctx, id, newName)
+	if err != nil {
+		return fmt.Errorf("dictionary.Service.RenameDictionary: %w", err)
+	}
+	return nil
 }
