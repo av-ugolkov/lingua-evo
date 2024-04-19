fc495a80c67ba6bfd26b410dae84f81099ffce08
aleksandr <ugolkov.prog@gmail.com>
2024-04-19 23:27:15 +0300

    create docker prod and fix test

--- internal/services/dictionary/service_test.go
+++ internal/services/dictionary/service_test.go
@@ -2,27 +2,32 @@ package dictionary
 
 import (
 	"context"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
 	"testing"
 
 	"github.com/av-ugolkov/lingua-evo/internal/services/dictionary/model"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
 func TestService_AddWord(t *testing.T) {
 	t.Run("AddWord", func(t *testing.T) {
 		var (
-			ctx  = context.Background()
-			data = model.WordRq{
+			ctx    = context.Background()
+			wordID = uuid.MustParse("efd91d3f-fd7e-4da3-9860-4e3a0012c887")
+			data   = model.WordRq{
 				Text:          "word",
 				Pronunciation: "[wɜ:d]",
 				LangCode:      "en",
 			}
 		)
 		repoWordMock := new(mockRepoDictionary)
-		repoWordMock.On("AddWords", ctx, mock.Anything).Return(mock.Anything, nil)
-		s := &Service{repo: repoWordMock}
+		repoWordMock.On("AddWords", ctx, mock.Anything).Return([]uuid.UUID{wordID}, nil)
+		langSvcMock := new(mockLangSvc)
+		langSvcMock.On("CheckLanguage", ctx, data.LangCode).Return(nil)
+
+		s := &Service{repo: repoWordMock, langSvc: langSvcMock}
 
 		got, err := s.AddWord(ctx, data)
 		assert.NoError(t, err)
