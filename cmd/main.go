56df8b36c68e18050243a6c71eb9d1e970d75015
aleksandr <ugolkov.prog@gmail.com>
2024-04-28 21:02:18 +0300

    add email into config

--- cmd/main.go
+++ cmd/main.go
@@ -13,9 +13,15 @@ func main() {
 	var configPath string
 	flag.StringVar(&configPath, "config", "./configs/server_config.yaml", "it's name of application config")
 
+	//var emailPassword string
+	//flag.StringVar(&emailPassword, "e-psw", "", "email password for newsletter")
+
 	flag.Parse()
 
 	slog.Info(fmt.Sprintf("config path: %s", configPath))
 	cfg := config.InitConfig(configPath)
+
+	//config.SetEmailPassword(emailPassword)
+
 	app.ServerStart(cfg)
 }
