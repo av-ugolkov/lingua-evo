cec23e504e8f3a985400dfa4e97e2309c294edfa
aleksandr <ugolkov.prog@gmail.com>
2024-05-17 19:35:26 +0300

    fix backup script

--- backup.sh
+++ backup.sh
@@ -2,6 +2,13 @@
 
 clear
 
-docker exec -t -i postgres-base /bin/bash -c "
-    cd /home/dump
-    pg_dump -U lingua pg-lingua-evo > backup_"$(date +'%d_%m_%Y_%H_%M_%S')".dump"
+docker exec -ti postgres-base /bin/bash -c "
+    if test -d /home/dumps; then
+        echo \"Directory [ dumps ] exists.\"
+    else
+        mkdir /home/dumps
+    fi
+    cd /home/dumps
+
+    pg_dump -U lingua pg-lingua-evo > backup_\"$(date +'%d_%m_%Y_%H_%M_%S')\".dump
+"
