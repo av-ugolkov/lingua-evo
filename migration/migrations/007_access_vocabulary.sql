9be326ae36f0aa4e782eb878e617950c9e562e14
aleksandr <ugolkov.prog@gmail.com>
2024-05-29 12:33:52 +0300

    fix migration

--- migration/migrations/007_access_vocabulary.sql
+++ migration/migrations/007_access_vocabulary.sql
@@ -16,11 +16,11 @@ ADD COLUMN IF NOT EXISTS "access" BIGINT NOT NULL DEFAULT 2;
 ALTER TABLE IF EXISTS "vocabulary"
 ADD COLUMN IF NOT EXISTS "access_edit" BOOLEAN NOT NULL DEFAULT FALSE;
 
-CREATE TABLE
-    IF EXISTS "vocabulary_users_access" (
+CREATE TABLE IF NOT EXISTS
+    "vocabulary_users_access" (
         "vocab_id" UUID REFERENCES "vocabulary" ("id") ON DELETE CASCADE,
         "subscriber_id" UUID REFERENCES "users" ("id") ON DELETE CASCADE,
-        "editor" BOOLEAN NOT NULL,
+        "editor" BOOLEAN NOT NULL
     );
 
 CREATE UNIQUE INDEX IF NOT EXISTS "idx_unique_vocabulary_users_access__vocab_id_subscriber_id" ON "vocabulary_users_access" ("vocab_id", "subscriber_id");
@@ -29,7 +29,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS "idx_unique_vocabulary_users_access__vocab_id_
 DROP TABLE IF EXISTS "access";
 
 ALTER TABLE IF EXISTS "vocabulary"
-DROP COLUMN IF NOT EXISTS "access";
+DROP COLUMN IF EXISTS "access";
 
 DROP TABLE IF EXISTS "vocabulary_users_access";
 
