b875cc22945112c538405d7a3637c4fcdee77c9d
aleksandr <ugolkov.prog@gmail.com>
2024-07-21 12:29:50 +0300

    remove v0

--- internal/delivery/handler/end_points.go
+++ internal/delivery/handler/end_points.go
@@ -1,37 +1,37 @@
 package handler
 
 const (
-	CurrentLanguage    = "/v0/current_language" //get
-	AvailableLanguages = "/v0/languages"        //get
+	CurrentLanguage    = "/current_language" //get
+	AvailableLanguages = "/languages"        //get
 
-	SignIn   = "/v0/auth/sign_in"   //post
-	Refresh  = "/v0/auth/refresh"   //get
-	SignOut  = "/v0/auth/sign_out"  //get
-	SendCode = "/v0/auth/send_code" //post
+	SignIn   = "/auth/sign_in"   //post
+	Refresh  = "/auth/refresh"   //get
+	SignOut  = "/auth/sign_out"  //get
+	SendCode = "/auth/send_code" //post
 
-	SignUp   = "/v0/user/sign_up" //post
-	UserByID = "/v0/user/id"      //get
+	SignUp   = "/user/sign_up" //post
+	UserByID = "/user/id"      //get
 
-	DictionaryWord = "/v0/dictionary/word"        //get post
-	GetRandomWord  = "/v0/dictionary/word/random" //get
+	DictionaryWord = "/dictionary/word"        //get post
+	GetRandomWord  = "/dictionary/word/random" //get
 
-	UserVocabulary   = "/v0/account/vocabulary"   //post, put, delete
-	UserVocabularies = "/v0/account/vocabularies" //get
+	UserVocabulary   = "/account/vocabulary"   //post, put, delete
+	UserVocabularies = "/account/vocabularies" //get
 
-	Vocabularies            = "/v0/vocabularies"           //get
-	Vocabulary              = "/v0/vocabulary"             //get
-	VocabularyCopy          = "/v0/vocabulary/copy"        //get
-	VocabularyAccessForUser = "/v0/vocabulary/access/user" //post delete
+	Vocabularies            = "/vocabularies"           //get
+	Vocabulary              = "/vocabulary"             //get
+	VocabularyCopy          = "/vocabulary/copy"        //get
+	VocabularyAccessForUser = "/vocabulary/access/user" //post delete
 
-	VocabularyWord        = "/v0/vocabulary/word"               //get post delete
-	VocabularyWordUpdate  = "/v0/vocabulary/word/update"        //post
-	VocabularyRandomWords = "/v0/vocabulary/words/random"       //get
-	VocabularyWords       = "/v0/vocabulary/words"              //get
-	WordPronunciation     = "/v0/vocabulary/word/pronunciation" //get
+	VocabularyWord        = "/vocabulary/word"               //get post delete
+	VocabularyWordUpdate  = "/vocabulary/word/update"        //post
+	VocabularyRandomWords = "/vocabulary/words/random"       //get
+	VocabularyWords       = "/vocabulary/words"              //get
+	WordPronunciation     = "/vocabulary/word/pronunciation" //get
 
-	VocabularyTags = "/v0/vocabulary/tag" //get
+	VocabularyTags = "/vocabulary/tag" //get
 
-	Accesses = "/v0/accesses" //get
+	Accesses = "/accesses" //get
 
-	Subscribers = "/v0/respondents" //get
+	Subscribers = "/respondents" //get
 )
