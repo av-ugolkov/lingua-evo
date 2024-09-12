3e9e805b48a3687070aaebb898f4b7d342b8f4d5
aleksandr <ugolkov.prog@gmail.com>
2024-09-12 15:21:01 +0300

    add version for docker containers

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
 
-clear
+dev() {
+    BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    COMMIT="$(git $dir rev-parse HEAD)"
+    echo $(
+        BRANCH=${BRANCH} \
+        COMMIT=${COMMIT} \
+        docker compose -p lingua-evo-dev -f deploy/docker-compose.dev.yml up --build --force-recreate
+    )
+        
+}
 
-if test -d /home/lingua-logs; then
-    echo "Directory [ lingua-logs ] exists."
-else
-    mkdir /home/lingua-logs
-fi
+release() {
+    clear
 
-if test -d /home/lingua-dumps; then
-    echo "Directory [ lingua-dumps ] exists."
-else
-    mkdir /home/lingua-dumps
-fi
+    if test -d /home/lingua-logs; then
+        echo "Directory [ lingua-logs ] exists."
+    else
+        mkdir /home/lingua-logs
+    fi
 
-echo "Do you want to create a backup for database? [y/n]"
-read ans
-if [ "$ans" = "y" ]; then
-    ./backup.sh
-fi
+    if test -d /home/lingua-dumps; then
+        echo "Directory [ lingua-dumps ] exists."
+    else
+        mkdir /home/lingua-dumps
+    fi
 
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-echo "Do you want to deploy [ $BRANCH ]? [y/n]"
-read ans
-if [ "$ans" = "n" ]; then
-    echo "Type branch name which you want yo use"
-    read BRANCH
-fi
-CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [ "$BRANCH" != "$CURRENT_BRANCH" ]; then
-    echo "$(git checkout $BRANCH)"
-fi
+    echo "Do you want to create a backup for database? [y/n]"
+    read ans
+    if [ "$ans" = "y" ]; then
+        ./backup.sh
+    fi
 
-EPSW="$(cat .env | xargs)"
+    BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    echo "Do you want to deploy [ $BRANCH ]? [y/n]"
+    read ans
+    if [ "$ans" = "n" ]; then
+        echo "Type branch name which you want yo use"
+        read BRANCH
+    fi
+    CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    if [ "$BRANCH" != "$CURRENT_BRANCH" ]; then
+        echo "$(git checkout $BRANCH)"
+    fi
 
-echo "$(git fetch)"
-echo "$(git pull)"
-echo "$(make run.docker $EPSW)"
+    EPSW="$(cat .env | xargs)"
+    COMMIT="$(git $dir rev-parse HEAD)"
+
+    echo "$(git fetch)"
+    echo "$(git pull)"
+    echo $(
+        EPSW=${EPSW} \
+        BRANCH=${BRANCH} \
+        COMMIT=${COMMIT} \
+        docker compose -p lingua-evo -f deploy/docker-compose.yml up --build --force-recreate
+    )
+}
+
+"$@"
