1995f21bf41e827e337c63a9c5da6988aba71278
aleksandr <ugolkov.prog@gmail.com>
2024-10-01 14:37:26 +0300

    create test for UserGetVocabularies

--- internal/db/transactor/transactor.go
+++ internal/db/transactor/transactor.go
@@ -2,11 +2,15 @@ package transactor
 
 import (
 	"context"
+	"errors"
 	"fmt"
+
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+var ErrCancelTx = errors.New("transaction canceled")
 
 type Transactor struct {
 	pgxPool *pgxpool.Pool
@@ -21,29 +25,39 @@ func NewTransactor(pgxPool *pgxpool.Pool) *Transactor {
 }
 
 func (t *Transactor) CreateTransaction(ctx context.Context, fn func(ctx context.Context) error) error {
-	tx, err := t.pgxPool.BeginTx(ctx, pgx.TxOptions{})
-	if err != nil {
-		return fmt.Errorf("db.Transactor.CreateTransaction: cannot begin transaction: %w", err)
+	var err error
+	tx := getExecutor(ctx)
+	if tx == nil {
+		tx, err = t.pgxPool.BeginTx(ctx, pgx.TxOptions{})
+		if err != nil {
+			return fmt.Errorf("db.Transactor.CreateTransaction: cannot begin transaction: %w", err)
+		}
+
+		defer func() {
+			switch p := recover(); {
+			case p != nil:
+				if rbErr := tx.Rollback(ctx); rbErr != nil {
+					err = fmt.Errorf("db.Transactor.WithTransaction: tx execute with panic:: transaction panic: %v, rollback err: %v", p, rbErr)
+				}
+			case err != nil:
+				if rbErr := tx.Rollback(ctx); rbErr != nil {
+					err = fmt.Errorf("db.Transactor.WithTransaction: transaction err: %v, rollback err: %v", err, rbErr)
+				}
+			default:
+				if err = tx.Commit(ctx); err != nil {
+					err = fmt.Errorf("db.Transactor.WithTransaction: cannot commit transaction: %v", err)
+				}
+			}
+		}()
+
+		err = fn(injectTx(ctx, tx))
+		if err != nil {
+			return err
+		}
+		return nil
 	}
 
-	defer func() {
-		switch p := recover(); {
-		case p != nil:
-			if rbErr := tx.Rollback(ctx); rbErr != nil {
-				err = fmt.Errorf("db.Transactor.WithTransaction: tx execute with panic:: transaction panic: %v, rollback err: %v", p, rbErr)
-			}
-		case err != nil:
-			if rbErr := tx.Rollback(ctx); rbErr != nil {
-				err = fmt.Errorf("db.Transactor.WithTransaction: transaction err: %v, rollback err: %v", err, rbErr)
-			}
-		default:
-			if err = tx.Commit(ctx); err != nil {
-				err = fmt.Errorf("db.Transactor.WithTransaction: cannot commit transaction: %v", err)
-			}
-		}
-	}()
-
-	err = fn(injectTx(ctx, tx))
+	err = fn(ctx)
 	if err != nil {
 		return err
 	}
