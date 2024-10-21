643eec01a5b961bdfb9059c6ed919b97ac36f6e3
aleksandr <ugolkov.prog@gmail.com>
2024-10-21 18:00:03 +0300

    delete password from configs

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log/slog"
 
 	"github.com/av-ugolkov/lingua-evo/internal/app"
 	"github.com/av-ugolkov/lingua-evo/internal/config"
@@ -11,13 +12,30 @@ func main() {
 	var configPath string
 	flag.StringVar(&configPath, "config", "./configs/server_config.yaml", "it's name of application config")
 
-	var emailPassword string
-	flag.StringVar(&emailPassword, "epsw", "", "email password for newsletter")
+	var emailPsw string
+	flag.StringVar(&emailPsw, "epsw", "", "email password for newsletter")
+
+	var jwtSecret string
+	flag.StringVar(&jwtSecret, "jwts", "", "")
+
+	var pgPsw string
+	flag.StringVar(&pgPsw, "pg_psw", "", "")
+
+	var redisPsw string
+	flag.StringVar(&redisPsw, "redis_psw", "", "")
 
 	flag.Parse()
 
+	if jwtSecret == "" || pgPsw == "" || redisPsw == "" {
+		slog.Error("empty jwts, pg_psw or redis_psw")
+		return
+	}
+
 	cfg := config.InitConfig(configPath)
-	config.SetEmailPassword(emailPassword)
+	config.SetEmailPassword(emailPsw)
+	config.SetJWTSecret(jwtSecret)
+	config.SetDBPassword(pgPsw)
+	config.SetRedisPassword(redisPsw)
 
 	app.ServerStart(cfg)
 }
